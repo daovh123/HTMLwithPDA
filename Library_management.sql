4208f39f3326a37141569080388e276392b58603
lehuyanh423015 <23021464@vnu.edu.vn>
2024-11-21 18:08:28 +0700

    Update Library_management.sql

--- Library_management.sql
+++ Library_management.sql
@@ -1,81 +1,81 @@
-drop database library_management;
+drop database if exists library_management;
 
 create database if not exists library_management;
 
 use library_management;
 
-create table book(
-    book_id int not null auto_increment,
-    book_name varchar(255),
-    author varchar(255),
-    published_date timestamp,
-    price double,
-    view int,
-    primary key (book_id)
-);
-
-create table users(
+create table members(
     userID int not null auto_increment,
-    username varchar(255) unique ,
-    firstName varchar(255),
-    lastName varchar(255),
-    password varchar(255),
-    email varchar(255),
-    phoneNum varchar(15),
+    username varchar(45) not null unique ,
+    firstName varchar(45),
+    lastName varchar(45),
+    sex varchar(10),
+    birth datetime,
+    email varchar(100),
+    phone varchar(15),
     primary key (userID)
 );
 
+create table admins (
+    admin_ID int not null auto_increment,
+    admin_name varchar(45) not null unique ,
+    firstName varchar(45),
+    lastName varchar(45),
+    password varchar(45),
+    sex varchar(10),
+    birth datetime,
+    email varchar(100),
+    phone varchar(15),
+    primary key (admin_ID)
+);
+
+create table books (
+    book_id varchar(10) not null ,
+    book_name varchar(45),
+    book_author varchar(45),
+    price double,
+    time_of_borrow int,
+    quantity_in_store int,
+    borrowing int,
+    remaining_quantity int,
+    primary key (book_id)
+);
+
 create table borrow (
+    ID int not null auto_increment,
     userID int not null,
-    username varchar(255),
+    username varchar(45),
     book_id int  not null ,
-    book_name varchar(255),
+    book_name varchar(45),
     date_out datetime,
     due_date datetime,
-    primary key (userID, book_id)
+    status varchar(45),
+    primary key (ID)
 );
 
 create table payment (
+    ID int not null auto_increment,
     userID int not null ,
-    username varchar(255),
+    username varchar(45),
     book_id int not null ,
-    book_name varchar(255),
+    book_name varchar(45),
     order_date datetime,
     quantity_of_order int not null,
     price double,
     payment double,
-    primary key (userID, book_id)
+    primary key (ID)
 );
 
 alter table borrow
     add constraint fk_userID_1
         foreign key (userID)
-        references users (userID)
+        references members (userID)
         on update cascade
-        on delete no action,
-    add constraint fk_book_id_1
-        foreign key (book_id)
-        references book (book_id)
-        on update cascade
-        on delete no action ;
+        on delete no action;
 
 alter table payment
     add constraint fk_userID_2
         foreign key (userID)
-        references users (userID)
+        references members (userID)
         on update cascade
-        on delete no action,
-    add constraint fk_book_id_2
-        foreign key (book_id)
-        references book (book_id)
-        on update cascade
-        on delete no action ;
-
-insert into users(username, firstName, lastName, password, email, phoneNum)
-    values ('huyanh', 'Le', 'Huy Anh', '23021463', '23021464@vnu.edu.vn', '0866502235');
-
-insert into users(username, firstName, lastName, password, email, phoneNum)
-    values ('Fanny', 'Zhuxin', 'Kagura', '23021464', 'lehuyanh423015@gmail.com', '0866502235');
-
-insert into users (username, firstName, lastName, password, email, phoneNum)
-    values ('Lylia', 'Lolita', 'Zhark', '23021464', 'lehuyanhk60@gmail.com', '0866502235');
+        on delete no action;
